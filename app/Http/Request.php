40c8f94817d1ef84167ce7ab35fba999ab81f24c
Faska <hichamfaska50@gmail.com>
2026-01-24 13:55:14 +0100

    Ajout d'une classe Request simple pour gérer les requêtes HTTP

---
+++ app/Http/Request.php
@@ -0,0 +1,39 @@
+<?php
+    namespace App\Http;
+
+    class Request{
+        public function __construct(
+            private array $query,
+            private array $request,
+            private array $server,
+            private array $files,
+            private array $cookies,
+        ){}
+
+        public static function capture():Request{
+            return new Request($_GET, $_POST, $_SERVER, $_FILES, $_COOKIE);
+        }
+
+        public function method():string{
+            return $this->server['REQUEST_METHOD'] ?? 'GET';
+        }
+
+        public function path():string{
+            $uri = $this->server['REQUEST_URI'] ?? '/';
+            return strtok($uri, '?');
+        }
+
+        public function input(string $key, mixed $default = null):mixed{
+            return $this->request[$key]
+                ?? $this->query[$key]
+                ?? $default;
+        }
+
+        public function file(string $key, mixed $default = null): mixed{
+            return $this->files[$key] ?? $default;
+        }
+
+        public function cookie(string $key, mixed $default = null): mixed{
+            return $this->cookies[$key] ?? $default;
+        }
+    }
